ea25019b530f26aacf43cbe58880e271aa135db5
wendell00 <wendellborges2003@gmail.com>
2022-10-08 22:50:40 -0300

    deixando cada vez mais funcional

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
+const amount = document.querySelector('.amount')
+let cash = 15
+let inGame = false
+updateCash()
+
 const opt1 = document.querySelector('.opt-1')
 const opt2 = document.querySelector('.opt-2')
 const optMiddle = document.querySelector('.option-middle')
+const betElement = document.querySelector('.betElement')
 let betDefined = ''
-
-const betInputValue = document.querySelector('.betInput').innerHTML
 
 opt1.addEventListener('click', (e) => {
     opt1.classList.toggle('selectedBet')
@@ -35,3 +39,39 @@ optMiddle.addEventListener('click', (e) => {
 function removeSelected(betOpt){
     betOpt.classList.remove('selectedBet')
 }
+
+
+const betInputValue = document.querySelector('.betInput')
+const btnBet = document.querySelector('.btnBet')
+
+btnBet.addEventListener('click', (e)=>{
+    e.preventDefault()
+    let valorApostado = betInputValue.value
+    if(Number(valorApostado)){
+        if(valorApostado <= cash && betDefined != ''){
+            cash = cash - valorApostado
+            updateCash()
+            betElement.classList.add('display-none')
+            inGame = true
+
+            if(inGame){
+                document.addEventListener('click', (e)=>{
+                    if(e.target.classList.contains('bg-card')){
+                        e.target.classList.add('display-none')
+                    }
+                })
+            }
+        } 
+    }
+})
+
+function updateCash(){
+    let cashString = cash.toString()
+    amount.innerHTML = cashString.search('.') == 0 ? cashString + '.00' : cashString
+}
+
+const cards = document.querySelectorAll('.card')
+
+// cards.addEventListener('click', (e)=>{
+//     console.log(e.target)
+// })
