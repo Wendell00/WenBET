a9e45ad2529687f2a22dc60ce980bbe28272547e
wendell00 <wendellborges2003@gmail.com>
2023-03-02 19:16:45 -0300

    Começando a Página

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import {NavBar} from './components/navbar'
 import './App.css'
 
 function App() {
-
+    return (
+        <div>
+            <NavBar></NavBar>
+        </div>
+    )
 }
 
 export default App
---
+++ src/components/navbar.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const NavBarStyles = styled.header`
+    width: 100vw;
+    height: 10vh;
+    min-height: 70px;
+    background-color: rgb(51, 51, 51, .7);
+    display: flex;
+    justify-content: center;
+    box-sizing: border-box;
+
+    .logo{
+      width: auto;
+      height: 100%;
+      object-fit: contain;
+    }
+
+    @media (min-width: 768px)
+    {
+      height: 15vh;
+    }
+`;
+
+
+export const NavBar = () =>{
+    return(
+      <NavBarStyles>
+        <div className="nav">
+          <img src="./src/assets/img/Logo.svg" className="logo"/>
+        </div>
+      </NavBarStyles>
+    )
+}
