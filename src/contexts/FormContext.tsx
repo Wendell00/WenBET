3225f1bc31879f60639625cd57132db8616282e8
wendell00 <wendellborges2003@gmail.com>
2023-04-02 21:26:29 -0300

    colocando useReducer no context

--- src/contexts/FormContext.tsx
+++ src/contexts/FormContext.tsx
@@ -1,6 +1,4 @@
-import { createContext, useState, ReactNode } from 'react'
-
-export const FormContext = createContext<FormContextData>({} as FormContextData);
+import { createContext, useReducer, ReactNode } from 'react'
 
 interface FormContextProviderProps {
   children: ReactNode
@@ -9,13 +7,46 @@ interface FormContextProviderProps {
 interface FormContextData {
   name: string;
   setName: (name: string) => void;
+  startBet: boolean;
+  setStartBet: (startBet: boolean) => void;
 }
 
+type Action =
+  | { type: 'SET_NAME', payload: string }
+  | { type: 'SET_START_BET', payload: boolean };
+
+function reducer(state: FormContextData, action: Action) {
+  switch (action.type) {
+    case 'SET_NAME':
+      return { ...state, name: action.payload };
+    case 'SET_START_BET':
+      return { ...state, startBet: action.payload };
+    default:
+      return state;
+  }
+}
+
+export const FormContext = createContext<FormContextData>({} as FormContextData);
+
 export function FormContextProvider({ children }: FormContextProviderProps) {
-  const [name, setName] = useState(' ')
+  const [state, dispatch] = useReducer(reducer, { 
+    name: '', setName: () => {},
+    startBet: false, setStartBet: () => {}
+    });
+
+  function setName(name: string) {
+    dispatch({ type: 'SET_NAME', payload: name });
+  }
+
+  function setStartBet(startBet: boolean) {
+    dispatch({ type: 'SET_START_BET', payload:startBet });
+  }
 
   return (
-    <FormContext.Provider value={{ name, setName }}>
+    <FormContext.Provider value={{ 
+      name: state.name, setName, 
+      startBet: state.startBet, setStartBet 
+      }}>
       {children}
     </FormContext.Provider>
   )
